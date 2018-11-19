34b695f679c97e5aea3465d18ec55b0ad797ba49
Biljana Jelaca <bilja.jelaca@gmail.com>
2018-11-19 21:02:51 +0100

    Add contact list

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ContactList /> <!-- dodajemo ContactList, da bi nam ispisao -->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ContactList from './components/ContactList.vue' //importujemo komponentu
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    ContactList //navodimo komponentu u listu komponenata, da bi mogli da je koristimo
   }
 }
 </script>
---
+++ src/components/ContactList.vue
@@ -0,0 +1,43 @@
+<!-- NASA KONTAKT LISTA -->
+<template>
+    <div>
+        <h3>Contact List</h3>
+
+        <table border=3>
+            <thead>
+                <th>Key</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+            </thead>
+            <tbody>
+                <!--ovaj key nam je redni broj za svakog usera; :key="key" je kljuc, tj index-->
+                <tr v-for="(contact, index) in contacts" :key="index">
+                    <td>{{index + 1}}</td> <!--index+1 -> da ne krece od 0 nego od 1-->
+                    <td>{{contact.firstName}}</td>
+                    <td>{{contact.lastName}}</td>
+                    <td>{{contact.email}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+</template>
+
+
+<script>
+export default {
+    //data je fnc koja vraca objekat
+    data() {
+        return {
+            contacts: [
+                {firstName: 'Saban', lastName:'Saulic', email: 'saban@gmail.com'},
+                {firstName: 'Mira', lastName:'Skoric', email: 'mira@gmail.com'},
+                {firstName: 'Lepa', lastName:'Lukic', email: 'lepa@gmail.com'},
+                {firstName: 'Zorica', lastName:'Brunclik', email: 'zorica@gmail.com'},
+                {firstName: 'Ilda', lastName:'Saulic', email: 'ilda@gmail.com'}
+            ]
+        };
+    }
+};
+</script>
